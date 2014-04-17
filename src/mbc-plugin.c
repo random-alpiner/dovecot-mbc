bbeb31eaa296be4366dad2133545d30aee63beb7
Lukas Mika <lukas.mika@web.de>
2014-04-17 10:35:48 +0200

    strdup

--- src/mbc-plugin.c
+++ src/mbc-plugin.c
@@ -51,7 +51,7 @@ mbc_mailbox_create(struct mailbox *box)
 	struct mbc_user *muser = MBC_USER_CONTEXT(box->storage->user);
 
 	char *directory;
-	const char prefix[box->list->ns->prefix_len] = box->list->ns->prefix;
+	const char *prefix = strdup(box->list->ns->prefix);
 	
 	if (mail_storage_is_mailbox_file(box->storage)) {
 		directory = mailbox_list_get_path(box->list, box->name,
