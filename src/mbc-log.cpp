e14c4673638b169bb13f980993e88a7652b52827
Lukas Mika <lukas.mika@web.de>
2014-10-18 01:48:03 +0200

    Update mbc-log.cpp
    Create or append log. Never truncate.

--- src/mbc-log.cpp
+++ src/mbc-log.cpp
@@ -5,29 +5,15 @@
 #include "system.h" // SysShutdown();
 #include "log.h"
 
-bool LogCreated = false;
-
-
 void Log (char *message) {
   FILE *file;
   
-  if (!LogCreated) {
-    file = fopen(LOGFILE, "w");
-    LogCreated = true;
-  } else 
-    file = fopen(LOGFILE, "a");
+  file = fopen(LOGFILE, "a");
     
-  if (file == NULL) {
-    if (LogCreated)
-      LogCreated = false;
-    return;
-  } else {
+  if (file) {
     fputs(message, file);
     fclose(file);
   }
-  
-  if (file)
-    fclose(file);
 }
 
 void LogErr (char *message) {
