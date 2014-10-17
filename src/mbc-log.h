af7856f104e6d793026be52839b63244214b14fa
Lukas Mika <lukas.mika@web.de>
2014-10-18 01:49:33 +0200

    Update mbc-log.h
    We don't need LogCreated anymore

--- src/mbc-log.h
+++ src/mbc-log.h
@@ -2,7 +2,5 @@
 
 #define LOGFILE "/var/log/mail.log" // all Log(); messages will be appended to this file
 
-extern bool LogCreated; // keeps track whether the log file is created or not
-
 void Log (char *message); // logs a message to LOGFILE
 void LogErr (char *message); // logs a message; execution is interrupted
