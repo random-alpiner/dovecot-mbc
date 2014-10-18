f472e7b79e1388fba57672aa2f8af9b8293f8019
Lukas Mika <lukas.mika@web.de>
2014-10-18 02:18:52 +0200

    Delete mbc-log.h

--- src/mbc-log.h
+++
@@ -1,6 +0,0 @@
-// log.h; the header file which defines Log(); and LogErr();
-
-#define LOGFILE "/var/log/mail.log" // all Log(); messages will be appended to this file
-
-void Log (char *message); // logs a message to LOGFILE
-void LogErr (char *message); // logs a message; execution is interrupted
